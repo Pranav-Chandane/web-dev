fccf5105e73697aab1c860182b51932bf62aa18c
Pranav chanadane <112601648+Pranav-Chandane@users.noreply.github.com>
2023-10-08 12:27:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+console.log("Hello World");
+
+// const element = document.getElementById("myDIV");
+// const list = element.querySelectorAll(".submitbtn");
+// list[0].style.backgroundColor = "red";
+
+// // function showMsg(){
+// //     alert("Form is submited");
+// // }
+
+// let submitbtn = document.querySelector('button');
+// button.addEventListener('click', showMsg);
+
+// function showMsg(){
+//     alert("Form is submited");
+// }
+
+// variables in javascript 
+// let x = prompt("Enter your number: ");
+// let y = x/x ; 
+// if (x == y) {
+//     console.log(y)
+// }
+
+function name(){
+    alert("your name is :");
+}
+
+function goToURL() {
+    Window.open('home.html');
+}
+var x = document.getElementById("myAudio")
+function playAudio() {
+    x.play();
+}
